bf2e1ec7ca62442dcb1d7baf2e4472e0836d5494
Dongbo Wang <dongbow@microsoft.com>
2025-11-05 15:21:03 -0800

    Support module functions as MCP tools

---
+++ MyMCPCommand.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Management.Automation;
+using MyFirstMCP.Tools;
+using System.Reflection.Metadata;
+
+namespace MyFirstMCP;
+
+[Cmdlet(VerbsLifecycle.Start, "MyMCP", DefaultParameterSetName = "Default")]
+public class MyMCPCommand : PSCmdlet
+{
+    private const string DefaultSet = "Default";
+    private const string ScriptToolSet = nameof(ScriptRoot);
+    private const string ModuleToolSet = nameof(Module);
+
+    [Parameter(Mandatory = true, ParameterSetName = ScriptToolSet)]
+    public string ScriptRoot { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = ModuleToolSet)]
+    public string Module { get; set; }
+
+    protected override void ProcessRecord()
+    {
+        Task mcpTask = Task.Run(async () => await StartMCPServer(ParameterSetName, ScriptRoot ?? Module));
+        mcpTask.GetAwaiter().GetResult();
+    }
+
+    static async Task StartMCPServer(string parameterSet, string scriptRootOrModule)
+    {
+        var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder([]);
+        builder.Logging.AddConsole(consoleLogOptions =>
+        {
+            // Configure all logs to go to stderr
+            consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+        });
+
+        IMcpServerBuilder mcpBuilder = builder.Services
+            .AddMcpServer()
+            .WithStdioServerTransport();
+
+        if (parameterSet is DefaultSet)
+        {
+            mcpBuilder.WithToolsFromAssembly();
+        }
+        else if (parameterSet is ScriptToolSet)
+        {
+            List<McpServerTool> tools = [];
+            if (Directory.Exists(scriptRootOrModule))
+            {
+                foreach (string file in Directory.EnumerateFiles(scriptRootOrModule, "*.ps1"))
+                {
+                    tools.Add(new PSScriptMcpServerTool(file));
+                }
+            }
+            mcpBuilder.WithTools(tools);
+        }
+        else
+        {
+            ModuleToolsMetadata metadata = new(scriptRootOrModule);
+            var tools = metadata.GetFunctionMcpTools();
+            mcpBuilder.WithTools(tools);
+        }
+
+        await builder.Build().RunAsync();
+    }
+}
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new([.. message.Reverse()]);
+}
---
+++ tools/PSToolUtils.cs
@@ -0,0 +1,173 @@
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Reflection;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Protocol;
+
+namespace MyFirstMCP.Tools;
+
+internal class PSToolUtils
+{
+    internal static CallToolResult GetErrorResult(string toolName, Exception ex)
+    {
+        StringBuilder sb = null;
+        string message = ex.Message;
+
+        while (ex.InnerException is { })
+        {
+            sb ??= new(message, capacity: message.Length * 3);
+            if (sb[^1] is not '\n')
+            {
+                sb.Append('\n');
+            }
+
+            sb.Append($"   Inner -> {ex.InnerException.Message}");
+            ex = ex.InnerException;
+        }
+
+        if (sb is not null)
+        {
+            message = sb.ToString();
+        }
+
+        string error = $"""
+            Failed to run the tool '{toolName}' due to the following error:
+            ```
+            {message}
+            ```
+            Check to see if it's caused by the passed-in command name or parameter name(s), and if so, please try again.
+            """;
+
+        return new CallToolResult()
+        {
+            Content = [new TextContentBlock { Text = error }],
+            IsError = true
+        };
+    }
+
+    internal static Tool CreateToolForScriptOrFunction(PowerShell pwsh, CommandInfo commandInfo)
+    {
+        var scriptInfo = commandInfo as ExternalScriptInfo;
+        var funcInfo = scriptInfo is null ? commandInfo as FunctionInfo : null;
+
+        if (scriptInfo is null && funcInfo is null)
+        {
+            throw new NotSupportedException("We only support .ps1 scripts or module functions as MCP server tools.");
+        }
+
+        string kind = scriptInfo is { } ? "script" : "function";
+        string name = commandInfo.Name;
+
+        if (commandInfo.ParameterSets.Count > 1)
+        {
+            throw new InvalidDataException($"The {kind} '{name}' cannot have more than 1 parameter set.");
+        }
+
+        dynamic help = pwsh
+            .AddCommand("Get-Help")
+            .AddParameter("Name", scriptInfo?.Source ?? funcInfo.Name)
+            .AddParameter("Full")
+            .Execute<PSObject>() ?? throw new InvalidDataException($"The {kind} '{name}' has no comment based help defined.");
+
+        string toolName = Path.GetFileNameWithoutExtension(name).Replace('-', '_');
+        string toolDescription = ValueOf<string>(help.description?[0]?.Text) ?? throw new InvalidDataException($"No description found for the {kind} '{name}'.");
+        PSObject[] parameters = ValueOf<PSObject[]>(help.parameters?.parameter) ?? Array.Empty<PSObject>();
+
+        JsonObject schema = new()
+        {
+            { "type", "object" },
+            { "additionalProperties", false },
+            { "$schema", "http://json-schema.org/draft-07/schema#" }
+        };
+
+        JsonObject parameterSchemas = [];
+        JsonArray requiredProperties = null;
+
+        foreach (dynamic parameter in parameters)
+        {
+            ParameterMetadata paramInfo = commandInfo.Parameters[ValueOf<string>(parameter.name)];
+            string paramDescription = ValueOf<string>(parameter?.description[0].Text);
+
+            var paramAttr = paramInfo.Attributes.OfType<ParameterAttribute>().FirstOrDefault();
+            object defaultValue = paramAttr.Mandatory
+                ? null
+                : GetDefaultValue(
+                    (scriptInfo?.ScriptBlock ?? funcInfo.ScriptBlock).Ast,
+                    paramInfo.Name,
+                    paramInfo.ParameterType);
+
+            var paramSchema = AIJsonUtilities.CreateJsonSchema(
+                type: paramInfo.ParameterType,
+                description: paramDescription,
+                hasDefaultValue: defaultValue is { },
+                defaultValue: defaultValue);
+
+            parameterSchemas.Add(paramInfo.Name, JsonSerializer.SerializeToNode(paramSchema));
+            if (paramAttr.Mandatory)
+            {
+                (requiredProperties ??= []).Add((JsonNode)paramInfo.Name);
+            }
+        }
+
+        schema.Add("properties", parameterSchemas);
+        if (requiredProperties is { })
+        {
+            schema.Add("required", requiredProperties);
+        }
+
+        return new Tool()
+        {
+            Name = toolName,
+            Description = toolDescription,
+            InputSchema = JsonSerializer.SerializeToElement(schema)
+        };
+    }
+
+    private static T ValueOf<T>(dynamic value)
+    {
+        if (value is T ret)
+        {
+            return ret;
+        }
+
+        object v = value is PSObject psobj ? psobj.BaseObject : value;
+        return (T)v;
+    }
+
+    private static object GetDefaultValue(Ast ast, string paramName, Type paramType)
+    {
+        var parameters = ast switch
+        {
+            FunctionDefinitionAst fdAst => fdAst.Parameters ?? fdAst.Body.ParamBlock?.Parameters,
+            ScriptBlockAst sbAst => sbAst.ParamBlock?.Parameters,
+            _ => throw new UnreachableException("Code not reachable.")
+        };
+
+        var paramAst = parameters?.FirstOrDefault(pAst => pAst.Name.VariablePath.UserPath == paramName);
+        if (paramAst is { } && paramAst.DefaultValue is ConstantExpressionAst constantAst)
+        {
+            return constantAst.Value;
+        }
+
+        if (paramType == typeof(int))
+        {
+            return 0;
+        }
+
+        if (paramType == typeof(string))
+        {
+            return string.Empty;
+        }
+
+        return typeof(PSScriptMcpServerTool)
+            .GetMethod(nameof(GetDefaultValueGeneric), BindingFlags.NonPublic | BindingFlags.Static)
+            .MakeGenericMethod(paramType)
+            .Invoke(null, null);
+    }
+
+    private static T GetDefaultValueGeneric<T>() => default;
+}
